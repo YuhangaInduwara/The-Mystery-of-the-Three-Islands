01848cf91dc8e01bcc5a61a34efbdc5ad1775d2f
YuhangaInduwara <yuhanga2001@gmail.com>
2024-09-05 15:28:38 +0530

    Created some changes

--- Assets/Scripts/RotationManager.cs
+++ Assets/Scripts/RotationManager.cs
@@ -5,23 +5,19 @@ public class RotationManager : MonoBehaviour
 {
     public float rotationSpeed = 50f;
     
-    // Assign the player object in the Inspector
     public GameObject player;
 
     void Update()
     {
-        // Rotate the object around the Z-axis (forward vector)
         transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
     }
 
-    // Detect player collision
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the colliding object is the player
         if (other.gameObject == player)
         {
-            // Load the next scene (Level_02)
-            SceneManager.LoadScene("Level_02");
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex + 1);
         }
     }
 }
